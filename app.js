4dc8ff94649a2719312c5b3963f7c79d6c0bb1c5
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2021-10-28 12:43:02 +0530

    javascript done

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 //alert("script is working")
-var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
-var outputBox=document.querySelector("#output");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputBox = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+//ferb-latin API
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
 
 //outputBox.innerText="bhavika"
 //console.log(outputBox)
-
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text
+}
+function errorhandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with server! try again after sometime");
+}
 function clickHandler() {
-   
-        outputBox.innerText= "xyxxyxyx" + txtInput.value
 
-    };
+    // outputBox.innerText= "xyxxyxyx" + txtInput.value
+    var inputText = txtInput.value;
+    if (inputText == "")
+        alert("Enter some text");
 
-    btnTranslate.addEventListener("click",clickHandler)
+    //calling server
+    else {
+        fetch(getTranslationURL(inputText))
 
- 
+            .then(response => response.json())
+            .then(json =>{
+                var translatedTxt=json.contents.translated;
+                console.log(translatedTxt);
+                outputBox.innerText=translatedTxt;
+            } )
+            .catch(errorHandler)
+    }
+
+};
+
+btnTranslate.addEventListener("click", clickHandler);
